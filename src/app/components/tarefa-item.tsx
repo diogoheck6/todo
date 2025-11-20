4a28cfd34f87516350c464368ef7bb70c7381a45
diogoheck6 <diogoheck6@gmail.com>
2025-11-20 09:12:27 -0300

    obtendo-tarefas

---
+++ src/app/components/tarefa-item.tsx
@@ -0,0 +1,16 @@
+import { Tarefa } from "@/core/model/Tarefa"
+import { IconTrash } from "@tabler/icons-react"
+
+export interface TarefaItemProps {
+	tarefa: Tarefa
+	excluir: (tarefa: Tarefa) => void
+}
+
+export default function TarefaItem(props: TarefaItemProps) {
+	return (
+		<li className="flex items-center bg-zinc-700 p-2 rounded-md">
+			<span className="flex-1">{props.tarefa.nome}</span>
+			<IconTrash className="text-red-500 hover:text-red-400 cursor-pointer" />
+		</li>
+	)
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,43 @@
 "use client"
+import obterTarefas from "@/backend/casos-uso/obter-tarefas"
 import salvarTarefa from "@/backend/casos-uso/salvar-tarefa"
 import { useState } from "react"
+import TarefaItem from "./components/tarefa-item"
 
 export default function Home() {
+	const [nome, setNome] = useState<any>("")
 	const [tarefa, setTarefa] = useState<any>(null)
+	const [tarefas, setTarefas] = useState<any>([])
 
-	async function click() {
-		const novaTarefa = await salvarTarefa({
-			nome: "Tarefa Criada pelo Frontend",
-		})
+	async function clickSalvar() {
+		const novaTarefa = await salvarTarefa({ nome })
 		setTarefa(novaTarefa)
+	}
+
+	async function clickObter() {
+		const tarefas = await obterTarefas()
+		setTarefas(tarefas)
 	}
 
 	return (
 		<div>
 			<h1>Lista de Tarefas</h1>
-			<button onClick={click} className="bg-blue-500 p-2">
-				Adicionar Tarefa
+			<input type="text" value={nome} onChange={(e) => setNome(e.target.value)} />
+			<button onClick={clickSalvar} className="bg-blue-500 p-2">
+				Salvar Tarefa
+			</button>
+			<button onClick={clickObter} className="bg-yellow-500 p-2">
+				Obter Tarefas
 			</button>
 			{tarefa && <p>{tarefa.id}</p>}
 			{tarefa && <p>{tarefa.nome}</p>}
+
+			<ul className="flex flex-col gap-4">
+				{tarefas.map((tarefa: any) => (
+					<TarefaItem key={tarefa.id} tarefa={tarefa} excluir={() => {}} />
+					// <li key={tarefa.id}>{tarefa.nome}</li>
+				))}
+			</ul>
 		</div>
 	)
 }
